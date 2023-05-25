124b28c7340c6182776badd1584bbc5de393c3a2
ShantanuBorkar <shantanu.b@arishti.in>
2023-05-25 11:50:22 +0530

    minor fix

--- lib/providers/map_utils.dart
+++ lib/providers/map_utils.dart
@@ -6,10 +6,10 @@ class MapUtils {
   static Future<void> openMap(double latitude, double longitude) async {
     String googleUrl =
         'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
-    if (await canLaunchUrl(Uri.parse(googleUrl))) {
+    try {
       await launchUrl(Uri.parse(googleUrl));
-    } else {
-      throw 'Could not open the map.';
+    } catch (e) {
+      throw Exception(e);
     }
   }
 }
